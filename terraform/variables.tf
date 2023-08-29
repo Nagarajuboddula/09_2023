dd9ac32f49213a320c64fb625a688c26acb547f1
Nagarajuboddula <naga.boddula@outlook.com>
2023-08-29 10:27:02 +0530

    send

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,4 @@
 variable "awsprops" {
-    type = "map"
-    default = {
-    region = "us-east-1"
-    ami = "ami-0c1bea58988a989155"
-    ami1 = ""
-    itype = "t2.micro"
-    publicip = true
-    keyname = "myseckey"
-    secgroupname = "IAC-Sec-Group"
-  }
+  description = " AWS region in which resources are deployhing"
+  type = string
 }
