a9eaf36e8f7d52ff19bc10c3e1f86a0b581f53b4
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-08 02:40:43 +0530

    flkjgh

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_example" {
 
-    ami = "ami-0767046d1677be5a0"
+    ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
     key_name= "lambda-test"
     #vpc_security_group_ids = [aws_security_group.main.id]
