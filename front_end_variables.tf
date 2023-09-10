bfc362f72456f03e584d69f0add77eaf45ada20a
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-10 20:15:11 +0530

    ingress

--- front_end_variables.tf
+++ front_end_variables.tf
@@ -36,7 +36,7 @@ description = "list of egress rules with cidr blocks to be allowed"
 type = list(map(string))
 default = []
 }*/
-
+/*
 variable "frontend_ingress_rules" {
   default     = {
     "description" = ["For HTTP", "For SSH"]
@@ -46,5 +46,26 @@ variable "frontend_ingress_rules" {
     "cidr_blocks" = ["0.0.0.0/0", "0.0.0.0/0"]
   }
   type        = map(list(string))
+  description = "Security group rules"
+}*/
+
+variable "frontend_ingress_rules" {
+  default     = {
+    "my ingress rule" = {
+      "description" = "For HTTP"
+      "from_port"   = "80"
+      "to_port"     = "80"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+    "my other ingress rule" = {
+      "description" = "For SSH"
+      "from_port"   = "22"
+      "to_port"     = "22"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    }
+  }
+  type        = map(any)
   description = "Security group rules"
 }
