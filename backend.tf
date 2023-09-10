1fc68b856bd5e2fc32b5b39d4d1c558ff0a7c7f4
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-10 20:03:18 +0530

    sg2

--- backend.tf
+++ backend.tf
@@ -1,5 +1,4 @@
 
-/*
 resource "aws_instance" "ec2_example" {
 
     ami = "ami-053b0d53c279acc90"
@@ -17,32 +16,5 @@ resource "aws_instance" "ec2_example" {
       sudo echo "<html><body><h1>Hello this custom page built with Terraform User Data</h1></body></html>" > /var/www/html/index.html
       EOF*/ 
 } 
-/*resource "aws_security_group" "main" {
-  egress = [
-    {
-      cidr_blocks      = [ "0.0.0.0/0", ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
-  }
-  ]
-}*/
+
 
