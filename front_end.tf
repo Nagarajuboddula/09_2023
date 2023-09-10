fa65957abba846987aee5750bbf509fae9823867
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-10 18:41:39 +0530

    push3

--- front_end.tf
+++ front_end.tf
@@ -1,26 +1,3 @@
-/*terraform {
-required_version = “>=0.12”
-}
-provider “aws” {
-#profile = var.profile
-shared_credentials_files = ["/root/.aws/credentials"]
-region = us-east-1
-}*/
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}*/
-
-
-
 
 resource "aws_instance" "frontend.ec2_instance" {
 
