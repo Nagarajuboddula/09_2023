e1b97930499b0a36fd5b731eb79e3175ac497df4
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-10 22:41:56 +0530

    add

--- front_end.tf
+++ front_end.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "frontend_ec2_instance" {
     instance_type = var.frontend_ec2_instance_type
     key_name= var.frontend_key_name
     tags = var.frontend_instance_tags
-    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    vpc_security_group_ids = [aws_security_group.sec-grp.id]
     #sg_description = var.frontend_sg_description
     #sg_ingress_with_cidr_blocks = var.frontend_sg_ingress_with_cidr_blocks
     #sg_egress_with_cidr_blocks = var.frontend_sg_egress_with_cidr_blocks
