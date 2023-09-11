e3e37507c5443228eb36b587ef05f49f50f80de6
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-11 14:31:49 +0530

    vol

--- front_end.tf
+++ front_end.tf
@@ -4,6 +4,10 @@ resource "aws_instance" "frontend_ec2_instance" {
     instance_type = var.frontend_ec2_instance_type
     key_name= var.frontend_key_name
     tags = var.frontend_instance_tags
+    root_block_device {
+      volume_size =15
+      volume_type = gp3
+    }
     vpc_security_group_ids = [aws_security_group.sec-grp.id]
     #sg_description = var.frontend_sg_description
     #sg_ingress_with_cidr_blocks = var.frontend_sg_ingress_with_cidr_blocks
