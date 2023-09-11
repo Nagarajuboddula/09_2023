6acfc3a72c55094777f0490ff9ef8f665e63e50a
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-11 20:31:31 +0530

    key

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "statefile-storage-demo"
-    key            = "/DEMO/DEMO2/NC_demo_terraform.tfstate"
+    key            = "DEMO/DEMO2/NC_demo_terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     kms_key_id     = "arn:aws:kms:us-east-1:932333216011:key/04f4d4fa-bc87-440b-a32e-c2835ef1aa90"
